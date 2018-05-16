c6bce8a130446e380b9cc1a7f3bc52e7f8f2cf46
Josh Stone <cuviper@gmail.com>
2018-05-16 12:19:47 -0700

    test i128 only on newer rustc

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -18,5 +18,15 @@ for feature in rand serde; do
   cargo test --verbose --no-default-features --features="$feature"
 done
 
-cargo build --all-features
-cargo test --all-features
+# test `i128` and all features together
+if [[ "$TRAVIS_RUST_VERSION" =~ ^(nightly|beta|stable)$ ]]; then
+  cargo build --verbose --features=i128
+  cargo test --verbose --features=i128
+
+  cargo build --all-features
+  cargo test --all-features
+else
+  # all except `i128`
+  cargo build --features="rand serde"
+  cargo test --features="rand serde"
+fi
