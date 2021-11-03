3ff6d82a77b56653c8cb7a7429909db3133f5ae9
Ben Lovy <ben@deciduously.com>
2021-11-03 11:24:09 -0400

    Unconditionally set has_i128

--- build.rs
+++ build.rs
@@ -1,10 +1,3 @@
-extern crate autocfg;
-use std::env;
-
 fn main() {
-    let ac = autocfg::new();
-
-    if ac.probe_type("i128") || env::var_os("CARGO_FEATURE_I128").is_some() {
-        println!("cargo:rustc-cfg=has_i128");
-    }
+    println!("cargo:rustc-cfg=has_i128");
 }
