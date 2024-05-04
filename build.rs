8123302a772a9a53e1b01114c913c7b315b20a23
Josh Stone <cuviper@gmail.com>
2024-05-04 11:42:30 -0700

    Generate the radix bases in `const` instead of `build.rs`

--- build.rs
+++ build.rs
@@ -1,8 +1,4 @@
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::Write;
-use std::path::Path;
 
 fn main() {
     let ptr_width = env::var("CARGO_CFG_TARGET_POINTER_WIDTH");
@@ -22,57 +18,4 @@ fn main() {
     }
 
     println!("cargo:rerun-if-changed=build.rs");
-
-    write_radix_bases().unwrap();
-}
-
-/// Write tables of the greatest power of each radix for the given bit size.  These are returned
-/// from `biguint::get_radix_base` to batch the multiplication/division of radix conversions on
-/// full `BigUint` values, operating on primitive integers as much as possible.
-///
-/// e.g. BASES_16[3] = (59049, 10) // 3¹⁰ fits in u16, but 3¹¹ is too big
-///      BASES_32[3] = (3486784401, 20)
-///      BASES_64[3] = (12157665459056928801, 40)
-///
-/// Powers of two are not included, just zeroed, as they're implemented with shifts.
-fn write_radix_bases() -> Result<(), Box<dyn Error>> {
-    let out_dir = env::var("OUT_DIR")?;
-    let dest_path = Path::new(&out_dir).join("radix_bases.rs");
-    let mut f = File::create(dest_path)?;
-
-    for &bits in &[16, 32, 64] {
-        let max = if bits < 64 {
-            (1 << bits) - 1
-        } else {
-            std::u64::MAX
-        };
-
-        writeln!(f, "#[deny(overflowing_literals)]")?;
-        writeln!(
-            f,
-            "pub(crate) static BASES_{bits}: [(u{bits}, usize); 257] = [",
-            bits = bits
-        )?;
-        for radix in 0u64..257 {
-            let (base, power) = if radix == 0 || radix.is_power_of_two() {
-                (0, 0)
-            } else {
-                let mut power = 1;
-                let mut base = radix;
-
-                while let Some(b) = base.checked_mul(radix) {
-                    if b > max {
-                        break;
-                    }
-                    base = b;
-                    power += 1;
-                }
-                (base, power)
-            };
-            writeln!(f, "    ({}, {}), // {}", base, power, radix)?;
-        }
-        writeln!(f, "];")?;
-    }
-
-    Ok(())
 }
